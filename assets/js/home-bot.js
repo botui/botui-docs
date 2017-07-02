bb13615959f9a26e0abf5762a7f93e5f13e43eae
Moin Uddin <me@moin.im>
2017-07-02 15:33:30 +0500

    home demo

--- assets/js/home-bot.js
+++ assets/js/home-bot.js
@@ -6,12 +6,7 @@ homeBot.message.add({
 }).then(function () {
   return homeBot.message.add({
     delay: 1500,
-    content: 'BotUI is sending these messages'
-  });
-}).then(function () {
-  return homeBot.message.add({
-    delay: 1500,
-    content: 'Wanna know more about what BotUI can do?'
+    content: 'Wanna see a demo of what BotUI can do?'
   });
 }).then(function () {
   return homeBot.action.button({
@@ -35,37 +30,11 @@ homeBot.message.add({
 
 var tutorial = function () {
   homeBot.message.add({
-    delay: 1500,
-    content: 'Cool. Wanna push some buttons?'
-  }).then(function () {
-    return homeBot.action.button({
-      delay: 1000,
-      buttons: [{
-        text: 'Button one',
-        value: 'one'
-      }, {
-        text: 'Button two',
-        value: 'two'
-      }]
-    });
-  }).then(function (res) {
-    return homeBot.message.add({
-      delay: 1000,
-      content: 'You pushed button ' + res.value
-    });
+    delay: 1000,
+    content: "Awesome! Here's a text input .."
   }).then(function () {
     return homeBot.message.add({
-      delay: 1000,
-      content: 'Cool!'
-    });
-  }).then(function () {
-    return homeBot.message.add({
-      delay: 1300,
-      content: "Now let's get some input .."
-    });
-  }).then(function () {
-    return homeBot.message.add({
-      delay: 1000,
+      delay: 1200,
       content: 'Whats your name?'
     });
   }).then(function () {
@@ -79,7 +48,45 @@ var tutorial = function () {
       delay: 1000,
       content: res.value + ' is a nice name!'
     });
-  });
+  }).then(function () {
+    return homeBot.message.add({
+      delay: 1400,
+      content: "Here's a number input."
+    });
+  }).then(function () {
+    return homeBot.message.add({
+      delay: 1000,
+      content: 'How about your age?'
+    });
+  }).then(function () {
+    return homeBot.action.text({
+      delay: 800,
+      value: '18',
+      sub_type: 'number',
+      placeholder: 'Your age'
+    });
+  }).then(function (res) {
+    return homeBot.message.add({
+      delay: 1000,
+      content: 'Cool.'
+    });
+  }).then(function (res) {
+    return homeBot.message.add({
+      delay: 1000,
+      content: 'Here is an obligatory (embeded) gif for ya'
+    });
+  }).then(function (res) {
+    return homeBot.message.add({
+      delay: 1000,
+      type: 'embed',
+      content: 'https://giphy.com/embed/LYDNZAzOqrez6'
+    });
+  }).then(function (res) {
+    return homeBot.message.add({
+      delay: 2000,
+      content: 'Now go on, explore the docs and build yourself a bot.'
+    });
+  }).then(end);
 };
 
 var end = function () {
